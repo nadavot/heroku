059c669a22a8b2936e7aa599b4ecccbe1b60acaa
nadavot <46422884+nadavot@users.noreply.github.com>
2019-01-06 12:28:17 -0500

    Update conference-recording.php

--- conference-recording.php
+++ conference-recording.php
@@ -1,18 +1,9 @@
 <?php
 
-/**
-* This section ensures that Twilio gets a response.
-*/
-
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>'; 
 echo '<Response></Response>'; // Place the desired response (if any) here.
 
-/**
-* This section actually sends the email.
-*/
-
-/* Your email address. */
 $to = "info@nadavot.com";
 $subject = "Conference Recording";
 $message = "A recording of your recent conference call has been generated.";
