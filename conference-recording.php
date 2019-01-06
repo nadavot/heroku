2da89f6e268373967875000e1cbd069a016ac754
nadavot <46422884+nadavot@users.noreply.github.com>
2019-01-06 12:26:11 -0500

    Update conference-recording.php

--- conference-recording.php
+++ conference-recording.php
@@ -19,3 +19,4 @@ $message = "A recording of your recent conference call has been generated.";
 $message .= "To listen to this message, please visit this URL: {$_REQUEST['RecordingUrl']}";
 $headers = "From: twilio@nadavot.com"; // Who should it come from?
 mail($to, $subject, $message, $headers);
+?>
