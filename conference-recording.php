f167a758dd72746f51e1cd8a3e26d145d620838c
nadavot <46422884+nadavot@users.noreply.github.com>
2019-01-07 12:25:50 -0500

    Update conference-recording.php

--- conference-recording.php
+++ conference-recording.php
@@ -6,16 +6,16 @@
 
 header('Content-type: text/xml');
 echo '<?xml version="1.0" encoding="UTF-8"?>'; 
-echo '<Response></Response>'; // Place the desired response (if any) here.
+echo '<Response>Hello</Response>'; // Place the desired response (if any) here.
 
 /**
 * This section actually sends the email.
 */
 
 /* Your email address. */
-$to = "david@nadavot.com";
-$subject = "Message from {$_REQUEST['From']}";
+$to = "davidkakon@gmail.com";
+$subject = "Conference Call Archive";
 //$message = "You have received a message calling from {$_REQUEST['From']}";
 $message = "To listen to this message, please visit this URL: {$_REQUEST['RecordingUrl']}";
-$headers = "From: davidkakon@gmail.com"; // Who should it come from?
+//$headers = "From: davidkakon@gmail.com"; // Who should it come from?
 mail($to, $subject, $message, $message1, $headers);
